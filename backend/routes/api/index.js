7125c0497c9adbec8755dfb7f32819b2702ebe51
matthewhasan <matthewhasan1218@gmail.com>
2021-05-04 11:35:39 -0500

    added routers

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,38 +1,9 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+router.use('/session', sessionRouter);
 
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        },
-    })
-    setTokenCookie(res, user);
-    return res.json({ user });
-}));
-
-router.get(
-    '/restore-user',
-    restoreUser,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
-
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
+router.use('/users', usersRouter);
 
 module.exports = router;
